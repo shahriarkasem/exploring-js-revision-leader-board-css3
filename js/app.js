fc4f62668b7e034c14143c3d6fa6bf258602eab9
Shahriar Kasem <shahriarkasem@gmail.com>
2022-02-16 11:57:21 +0600

    task two done

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// task 1
+document.getElementById('top-players-title').style.color = 'rgba(255, 99, 71, 1)';
+document.getElementById('top-blogs-title').style.color = 'rgba(255, 99, 71, 1)';
+
+// task 2
+document.getElementById('players').style.backgroundColor = 'rgba(255, 99, 71, 0.2)';
+// extra decoration
+document.getElementById('players').style.padding = '15px 20px 15px 20px';
+document.getElementById('players').style.borderRadius = '10px';
+
+// task 3
+
+// extra part
+document.getElementById('book-list-article').style.textDecorationLine = 'none';
+// document.getElementById('book-list-article').style.paddingLeft = '25px';
+
+// task 4
+
+
+// task 5
+
+
+// task 6
+
+
+// task 7
+
+
+// task 8
+
+
+// task 9
+
